5821f26813c327b00919ed87f2488522602f9c0d
latif005 <latifwibowo005@gmail.com>
2025-03-01 11:41:01 +0700

    Add files via upload

---
+++ variabel_user.php
@@ -0,0 +1,10 @@
+<?php
+    // Mendefinisikan variabel
+    $nama = "Latif";
+    $umur = 19;
+    $bb = 60;
+
+    echo "Nama saya adalah : " . $nama;
+    echo "<br/>Umur saya adalah : " .$umur. " Tahun";
+    echo "<br/>Berat badan saya adalah : " .$bb. " Kg";
+?>
